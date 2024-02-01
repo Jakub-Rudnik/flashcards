642d99bfefbdaed3b6956171261621fbb69962ec
jakub-rudnik <jakub.rudnik@interia.pl>
2024-02-01 19:46:29 +0100

    Add shadcn ui and basic components

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import {ThemeToggle} from "@/components/themeToggle";
+
+export function Header() {
+    return (
+    <div className="flex h-16 items-center justify-between px-10 border-b-2">
+        <h4 className="text-xl font-semibold">Flashcards</h4>
+        <ThemeToggle />
+    </div>
+    );
+}
