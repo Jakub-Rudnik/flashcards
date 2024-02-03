6796847ef577d99403cef6308711a75f562344bf
jakub-rudnik <jakub.rudnik@interia.pl>
2024-02-03 23:38:29 +0100

    Add separator component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
-import Image from 'next/image'
-import {getFlashCards} from "@/db";
+import Image from 'next/image';
+import {getFlashCards} from '@/db';
+import {AddFlashcard} from '@/components/addFlashcard';
+import {Separator} from '@/components/ui/separator';
 
 export default async function Home() {
   const data = await getFlashCards();
 
-  console.log(data)
+  console.log(data);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <div className="flex flex-col items-center justify-center w-[640px]">
+        <AddFlashcard />
+        <Separator className="my-12" />
+        <div>
+          {data.map(flashcard => (
+            <div key={flashcard.id}>
+              <h1>{flashcard.front_side}</h1>
+              <p>{flashcard.back_side}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
-  )
+  );
 }
